2f1eba465eead330aa76c7a46002f92691316972
jensfinnas <jens.finnas@gmail.com>
2015-08-28 11:40:54 +0200

    Add logo as font

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var app = express()
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
 
+app.use(express.static('static'))
+
 app.get('/', function (req, res) {
   res.render('index',
   { title : 'Home' }
