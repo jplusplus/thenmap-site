cec687d5fe87ed9ef852dccc0da2b6f1d3f9ccd4
rotsee <leo_wallentin@hotmail.com>
2015-09-02 15:51:44 +0200

    add demo page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var express = require('express')
 var favicon = require('serve-favicon');
+var request = require('request');
 
 var app = express()
 app.set('view engine', 'jade')
@@ -10,13 +11,49 @@ app.set('views', __dirname + '/views')
 app.use(express.static('static'))
 app.use(favicon(__dirname + '/static/favicons/favicon.ico'))
 
+var availableDatasets = ['world-2', 'se-4', 'se-7']
+app.get('/demo', function(req, res) {
+
+  if (req.query.dataset && (availableDatasets.indexOf(req.query.dataset) > -1)){
+  	var dataset = req.query.dataset
+  } else {
+	var dataset = availableDatasets[0]
+  }
+
+  if (req.query.date){
+  	var date = new Date(req.query.date).toISOString()
+  } else {
+	var date = new Date().toISOString()
+  }
+  date = date.split("T")[0]
+
+  var apiUrl = "http://api.thenmap.net/v1/" + dataset + "/info"
+  request(apiUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+
+      var datasetInfo = JSON.parse(body).info
+
+	  if (req.query.language && (datasetInfo.languages.indexOf(req.query.language) > -1)){
+	  	var activeLanguage = req.query.language
+	  } else {
+		var activeLanguage = datasetInfo.defaultLanguage
+	  }
+
+      res.render('demo',{
+  	    availableDatasets: availableDatasets,
+  	    activeDataset: dataset,
+  	    datasetInfo: datasetInfo,
+  	    date: date,
+  	    activeLanguage: activeLanguage
+      })
+    }
+  })
+
+})
+
 app.get('/', function(req, res) {
   res.render('index')
 })
-
-/*app.get('/demo', function(req, res) {
-  res.render('demo')
-})*/
 
 // Errors
 app.use(function(req, res, next) {
